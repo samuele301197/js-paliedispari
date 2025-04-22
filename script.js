b325d7683921ee2e1ef4baf4adeeb2c586cc0f91
samuele301197 <parisisamu32@gmail.com>
2025-04-22 16:12:28 +0200

    feat:dispari

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 
-
+const oddEven = prompt("Pari o dispari?");
+console.log(oddEven);
 const number = prompt("Dimmi un numero da 1 a 5");
-const random = Math.floor(Math.random() * 5) + 1;
 console.log(number)
+somma();
+
+
+
+
+
+function somma() {
+    const random = Math.floor(Math.random() * 5) + 1;
+    console.log(random);
 }
-
-
